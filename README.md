9e1f8da7b7f729acc24d30a05de1efd81a79454b
Sam <moltinginstar@gmail.com>
2024-12-23 20:12:45 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Tupper
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+This is a simple command-line tool to plot text using [Tupper's self-referential formula](https://en.wikipedia.org/wiki/Tupper%27s_self-referential_formula).
+
+## Usage
+
+First, install the requirements:
+
+```sh
+pip install -r requirements.txt
+```
+
+Then, run the scrip like this:
+
+```sh
+python tupper.py "Hello, World!" -o hello_world.png
+```
+
+This will generate a PNG file with the text `"Hello, World!"`.
+
+## License
+
+Tupper is licensed under the [MIT License](LICENSE).
---
+++ tupper.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+"""Plot text using Tupper's formula.
+
+Example usage:
+  $ python3 tupper.py "Hello, World!" -o hello_world.png
+"""
+
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw, ImageFont
+from pathlib import Path
+
+
+SIZE = (106, 17)
+
+
+def tupper(x: int, y: int) -> bool:
+  """Tupper's self-referential formula."""
+  return 0.5 < ((y // 17) // (2 ** (17 * x + y % 17))) % 2
+
+
+def plot_text(text: str) -> int:
+  """Plot the given text using Tupper's formula.
+
+  Args:
+    text: The text to plot.
+
+  Returns:
+    The computed k value.
+  """
+  font = ImageFont.load_default(size=7)
+
+  image = Image.new("1", SIZE, color=0)
+  draw = ImageDraw.Draw(image)
+  draw.text((SIZE[0] // 2, SIZE[1] // 2), text, font=font, spacing=-1, fill=1, anchor="mm")
+
+  binary_matrix = np.array(image, dtype=int)
+  for row in binary_matrix:
+    print("".join(str(x) for x in row))
+
+  binary_matrix_flat = binary_matrix[:, ::-1].flatten("F")
+
+  k = 0
+  for bit in binary_matrix_flat:
+    k = (k << 1) | int(bit)
+  k *= 17
+
+  return k
+
+
+def save_tupper_to_image(k: int, path: Path) -> None:
+  """Save the plot of Tupper's formula for the given k value to an image.
+
+  Args:
+    k: The k value representing the text.
+    path: The path to save the image.
+  """
+  fig, ax = plt.subplots()
+  for y in range(SIZE[1]):
+    for x in range(SIZE[0]):
+      if tupper(x, y + k):
+        ax.plot(x, y, "ks", markersize=1)
+
+  ax.set_aspect("equal")
+  ax.set_xlim(-1, SIZE[0] + 1)
+  ax.set_ylim(-1, SIZE[1] + 1)
+  ax.axis("off")
+
+  fig.savefig(path, dpi=300)
+  plt.close()
+
+
+if __name__ == "__main__":
+  import argparse
+
+  parser = argparse.ArgumentParser(
+    prog="tupper",
+    description="Plot text using Tupper's formula.",
+  )
+  parser.add_argument("-o", "--output", type=Path, help="Output file. If not provided, the plot will not be saved.")
+  parser.add_argument("-v", "--verbose", action="store_true", help="Print k value.", default=False)
+  parser.add_argument("text", type=str, help="Text to plot.")
+  args = parser.parse_args()
+
+  text = args.text.strip()
+
+  k = plot_text(args.text)
+
+  if args.verbose:
+    print("\n---")
+    print(f"{k = } (# digits: {math.ceil(math.log10(k + 1))})")
+
+  if args.output:
+    save_tupper_to_image(k, args.output)
